e0987f83dcd22ed2d580532fd0ac71281a853a67
Matthew Soulanille <matthew@soulanille.net>
2017-07-16 15:17:36 -0700

    reading resource names

--- resourceFork.js
+++ resourceFork.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var Promise = require("bluebird");
 var path = require('path');
 var fs = require('fs');
 
@@ -30,12 +31,85 @@ class resourceFork {
 	var l_data = this.dataView.getUint32(2*4);
 	var l_map = this.dataView.getUint32(3*4);
 
+	// Get resource map
 	this.resource_data = new DataView(this.buffer, o_data, l_data);
 	this.resource_map = new DataView(this.buffer, o_map, l_map);
 	
-	// Parse resource map
-	var o_type_list = this.dataView.getUint16(24);
-	var o_name_list = this.dataView.getUint16(26);
+	// Get type and name list
+	// Make sure to account for the resource map's byteOffset
+	var o_type_list = this.resource_map.getUint16(24) + this.resource_map.byteOffset;
+	var o_name_list = this.resource_map.getUint16(26) + this.resource_map.byteOffset;
+	this.type_list = new DataView(this.buffer, o_type_list, o_name_list - o_type_list);
+	this.name_list = new DataView(this.buffer, o_name_list); // continues to end of buffer
+
+	// Type List
+	// 2 bytes: Number of resource types in the map minus 1 (no one uses resource fork without using at
+	// least one resource, so they get an extra type by doing this)
+	this.n_types = (this.type_list.getUint16(0) + 1) & 0xffff; // keep within uint16
+
+
+	this.resources = {};
+	
+	// read each resource
+	for (var i = 0; i < this.n_types; i++) {
+	    var resource_type_array = [this.type_list.getUint8(2 + 8*i),
+				       this.type_list.getUint8(3 + 8*i),
+				       this.type_list.getUint8(4 + 8*i),
+				       this.type_list.getUint8(5 + 8*i)];
+	    var resource_type = this.decode_macroman(resource_type_array);
+
+	    var quantity = this.type_list.getUint16(6 + 8*i) + 1;
+	    var offset = this.type_list.getUint16(8 + 8*i);
+
+
+	    if (this.resources.hasOwnProperty(resource_type)) {
+		throw "Duplicate resource type " + resource_type;
+	    }
+	    this.resources[resource_type] = [];
+
+	    for (var j = 0; j < quantity; j++) {
+		var res = {};
+		res.type = resource_type;
+		res.id = this.type_list.getUint16(offset + 12*j);
+
+
+		var o_name = this.type_list.getUint16(offset + 12*j + 2);
+		if (o_name == 0xffff) {
+		    res.name = "";
+		}
+		else {
+		    var name_len = this.name_list.getUint8(o_name);
+		    var name_list = [];
+		    for (var k = 0; k < name_len; k++) {
+			name_list.push(this.name_list.getUint8(o_name + 1 + k));
+		    }
+		    res.name = this.decode_macroman(name_list);
+		}
+
+		
+		//var attrs = this.type_list.getUint8(offset + 12*j + 4);
+		
+		var tmsb = this.type_list.getUint8(offset + 12*j + 5);
+		var t = this.type_list.getUint16(offset + 12*j + 6);
+
+		var o_rdat = (tmsb << 16) + t;
+		var l_rdat = this.resource_data.getUint32(o_rdat);
+		res.data = new DataView(this.buffer,
+					this.resource_data.byteOffset + o_rdat + 4,
+					l_rdat);
+
+		
+
+		this.resources[resource_type][res.id] = res;
+	    }
+	    
+
+	    
+
+	}
+	
+	
+	
 	
     }
     
@@ -54,8 +128,26 @@ class resourceFork {
 	}.bind(this));
     }
 
+    // see https://gist.github.com/jrus/3113240
+    decode_macroman(mac_roman_bytearray) {
+	var byte, char_array, idx;
+	var high_chars_unicode = 'ÄÅÇÉÑÖÜáàâäãåçéèêëíìîïñóòôöõúùûü\n†°¢£§•¶ß®©™´¨≠ÆØ∞±≤≥¥µ∂∑∏π∫ªºΩæø\n¿¡¬√ƒ≈∆«»… ÀÃÕŒœ–—“”‘’÷◊ÿŸ⁄€‹›ﬁﬂ\n‡·‚„‰ÂÊÁËÈÍÎÏÌÓÔÒÚÛÙıˆ˜¯˘˙˚¸˝˛ˇ'.replace(/\n/g, '');
 
-    
+	char_array = (function() {
+            var i, ref, results;
+            results = [];
+            for (idx = i = 0, ref = mac_roman_bytearray.length; 0 <= ref ? i < ref : i > ref; idx = 0 <= ref ? ++i : --i) {
+		byte = mac_roman_bytearray[idx];
+		if (byte < 0x80) {
+		    results.push(String.fromCharCode(byte));
+		} else {
+		    results.push(high_chars_unicode.charAt(byte - 0x80));
+		}
+            }
+            return results;
+	})();
+	return char_array.join('');
+    }
 
 
 }
--- test/test.js
+++ test/test.js
@@ -12,16 +12,31 @@ describe('Array', function() {
 
 
 describe("resourceFork", function() {
-
-
+    var rf;
+    before(function(done) {
+	rf = new resourceFork("./test/test.ndat", false);
+	// pretend data fork is resource fork for compatability with other OSes
+	rf.read().then(function() {done();});
+	
+    });
     
     describe("#constructor()", function() {
 	it('should properly append the resource fork to the given path', function() {
-	    var rf = new resourceFork("some/example/path");
-	    assert.equal("some/example/path/..namedfork/rsrc", rf.path);
+	    var test_rf = new resourceFork("some/example/path");
+	    assert.equal("some/example/path/..namedfork/rsrc", test_rf.path);
 	});
 
     });
 
+    describe("read()", function() {
+	it("should get the types of resources", function() {
+	    assert(rf.resources.hasOwnProperty("wëap"));
+	});
+	
+	it("should get the names of resources", function() {
+	    assert.equal('blaster', rf.resources['wëap'][128].name);
+	});
 
+    });
 });
+
