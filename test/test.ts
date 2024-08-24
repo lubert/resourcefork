f177e3b94389a870df05bc5773f37bf86664036a
Lu Zhu <misterzhu@gmail.com>
2024-08-23 23:20:18 -0700

    Fix tests

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,5 @@
-import { readResourceFork, ResourceMap } from "../src/resourceFork";
+import ResourceMap from "../src/ResourceMap";
+import { readResourceFork } from "../src/resourceFork";
 
 let resources: ResourceMap;
 
@@ -14,14 +15,14 @@ describe("resourceFork", () => {
     });
 
     it("should get the names of resources", () => {
-      expect(resources["wëap"][128].name).toBe("blaster");
+      expect(resources["wëap"]?.[128].name).toBe("blaster");
     });
 
     it("should get the data from the resources", () => {
       const data =
         "ff ff 00 1e 00 ea 00 7b ff ff 00 01 ff ff ff ff 00 00 ff ff 01 59 ff ff 00 00 00 00 00 00 00 00 ff ff 00 00 ff ff ff ff ff ff ff ff 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ff ff 00 00 00 00 ff ff 00 00 00 00 ff ff ff ff ff ff 00 00 00 00 00 00 ff ff ff ff ff ff 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ff ff 00 00 ff ff 00 00 00 00 ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff";
 
-      expect(resources["wëap"][128].hexString).toBe(data);
+      expect(resources["wëap"]?.[128].hexString).toBe(data);
     });
   });
 });
