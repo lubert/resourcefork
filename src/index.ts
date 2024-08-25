4b624bb6922149a565854518b610f4b5e270d054
Lu Zhu <misterzhu@gmail.com>
2024-08-25 14:56:53 -0700

    Add ResourceFork to exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Resource from "./Resource";
+import ResourceFork from "./ResourceFork";
 import { ResourceMap, ResourceHeader } from "./types";
 import { decodeMacRoman } from "./utils";
 
-export { Resource, ResourceHeader, ResourceMap, decodeMacRoman };
+export { Resource, ResourceFork, ResourceHeader, ResourceMap, decodeMacRoman };
